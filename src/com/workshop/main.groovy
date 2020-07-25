65c6a6a2e069669fcfff2385e8ef74bba401aad2
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2020-07-25 15:56:24 +0700

    add build stage
    Signed-off-by: Muhammad Muhlas <muhammadmuhlas3@gmail.com>

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -8,6 +8,7 @@ def main(script) {
    // Object initialization
    c = new Config()
    sprebuild = new prebuild()
+   sbuild = new build()
  
    // Pipeline specific variable get from injected env
    // Mandatory variable will be check at details & validation steps
@@ -17,11 +18,13 @@ def main(script) {
    def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
    def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
    def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
-
+ 
+   // Have default value
+   def docker_registry = ("${script.env.docker_registry}" != "null") ? "${script.env.docker_registry}" : "${c.default_docker_registry}"
+ 
    // Initialize docker tools
-   def dockerTool = tool name: 'docker', type: 'dockerTool'
-
-	
+  def dockerTool = tool name: 'docker', type: 'dockerTool'
+ 
    // Pipeline object
    p = new Pipeline(
        repository_name,
@@ -30,13 +33,12 @@ def main(script) {
        docker_user,
        app_port,
        pr_num,
-       dockerTool
-       
- 
+       dockerTool,
+       docker_registry
    )
  
    ansiColor('xterm') {
-   	   stage('Pre Build - Details') {
+       stage('Pre Build - Details') {
            sprebuild.validation(p)
            sprebuild.details(p)
        }
@@ -45,9 +47,9 @@ def main(script) {
            sprebuild.checkoutBuildTest(p)
        }
  
-       //stage('Build & Push Image') {
-           // TODO: Call build & push image function
-       //}
+       stage('Build & Push Image') {
+           sbuild.build(p)
+       }
  
        //stage('Merge') {
            // TODO: Call merge function
@@ -62,5 +64,5 @@ def main(script) {
        //}
    }
 }
-
+ 
 return this
